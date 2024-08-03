977a91727f0ec1507085f0cdf7a59d7cf93918cf
igorSerodio <igorserodioqueiroz@gmail.com>
2024-08-02 23:44:05 -0300

    Lab 3 (parcial)

---
+++ prisma/migrations/20240803022955_na_minha_geladeira0_1/migration.sql
@@ -0,0 +1,69 @@
+-- CreateEnum
+CREATE TYPE "TipoDeMedida" AS ENUM ('UNIDADES', 'GRAMAS', 'MILILITROS');
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashSenha" TEXT NOT NULL,
+    "apelido" TEXT,
+    "adm" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Receita" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT,
+    "idDoUsuario" INTEGER NOT NULL,
+
+    CONSTRAINT "Receita_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Ingrediente" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "tipoDeMedida" "TipoDeMedida" NOT NULL,
+
+    CONSTRAINT "Ingrediente_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "IngredienteReceita" (
+    "idDaReceita" INTEGER NOT NULL,
+    "idDoIngrediente" INTEGER NOT NULL,
+    "quantidade" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "IngredienteReceita_pkey" PRIMARY KEY ("idDaReceita","idDoIngrediente")
+);
+
+-- CreateTable
+CREATE TABLE "Avaliacao" (
+    "idDoUsuario" INTEGER NOT NULL,
+    "idDaReceita" INTEGER NOT NULL,
+    "nota" DOUBLE PRECISION NOT NULL,
+    "comentario" TEXT,
+
+    CONSTRAINT "Avaliacao_pkey" PRIMARY KEY ("idDoUsuario","idDaReceita")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+
+-- AddForeignKey
+ALTER TABLE "Receita" ADD CONSTRAINT "Receita_idDoUsuario_fkey" FOREIGN KEY ("idDoUsuario") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IngredienteReceita" ADD CONSTRAINT "IngredienteReceita_idDaReceita_fkey" FOREIGN KEY ("idDaReceita") REFERENCES "Receita"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IngredienteReceita" ADD CONSTRAINT "IngredienteReceita_idDoIngrediente_fkey" FOREIGN KEY ("idDoIngrediente") REFERENCES "Ingrediente"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Avaliacao" ADD CONSTRAINT "Avaliacao_idDoUsuario_fkey" FOREIGN KEY ("idDoUsuario") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Avaliacao" ADD CONSTRAINT "Avaliacao_idDaReceita_fkey" FOREIGN KEY ("idDaReceita") REFERENCES "Receita"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
